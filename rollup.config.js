a86f66ee7e002291c599ef039f0a201d08cc817d
luiz azevedo <lwnazevedo@gmail.com>
2022-02-19 18:30:26 -0300

    feat: adjusted imports

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -23,13 +21,10 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
     ],
-    external: ["react", "react-dom"],
   },
   {
     input: "dist/esm/types/index.d.ts",
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import Button from "./components/Button";
+export * from './components';
 
-export { Button };
