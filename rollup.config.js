1db1a2e1f0802b2a605d7e9567f7ac5a0212f478
luiz azevedo <lwnazevedo@gmail.com>
2022-02-21 16:59:19 -0300

    feat: new config rollup map

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 const packageJson = require("./package.json");
 
